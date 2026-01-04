5691cfe0a48d196efc368f4bafa4a7ed338818d4
salazaroliveros-prog <salazaroliveros@gmail.com>
2026-01-04 09:38:22 -0600

    Enhance Vite config with build options
    Added build configuration and public directory settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegúrate de que Vercel sirva esta carpeta
+    emptyOutDir: true,
+  },
+  // Si tienes assets globales, ponlos en /frontend/public
+  // Vite los copiará automáticamente al build final
+  publicDir: 'frontend', // Es el valor por defecto, pero puedes especificarlo explícitamente
 })
